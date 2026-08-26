adbb6affb6a2023f91ae81052e0ffd5dc5cc57f3
joaovictoreverscordeiro <joaovictorevers@gmail.com>
2026-08-26 00:16:44 -0300

    parte JV relas2

--- matlab/latextab.m
+++ matlab/latextab.m
@@ -1,56 +1,64 @@
 function latextab(T)
-    fpw = fopen("latextab.txt", "w");
+    if ~istable(T)
+        error('O argumento de entrada deve ser uma table.');
+    end
 
-    %numero de colunas
+    fpw = fopen("latextab.txt", "w");
+    if fpw == -1
+        error('Não foi possível criar o arquivo latextab.txt.');
+    end
+
     ncol = width(T);
     nrow = height(T);
 
-    %cabeçalho do tabular
-    fprintf(fpw, "\\begin{table}[H] \n \t\\centering\n\n \t\\caption{<<inserir legenda da tabela>>} \n \t\\label{<<inserir referência da tabela>>} \n\n \t\\begin{tabular}{");
+    fprintf(fpw, "\\begin{table}[H]\n\t\\centering\n\n\t\\caption{<<inserir legenda da tabela>>}\n\t\\label{<<inserir referência da tabela>>}\n\n\t\\begin{tabular}{");
 
     for i = 1:ncol
         fprintf(fpw, "c");
-
-        if (i == ncol)
+        if i == ncol
             fprintf(fpw, "}\n");
         else
             fprintf(fpw, " ");
         end
     end
 
-    %cabeçalho da tabela
     fprintf(fpw, "\t\t\\toprule\n\t\t");
+    
+    varNames = T.Properties.VariableNames;
     for i = 1:ncol
-        fprintf(fpw, "%s ", string(T.Properties.VariableNames(i)));
-
-        if (i == ncol)
-            fprintf(fpw, "\\\\ \n");
+        fprintf(fpw, "%s", varNames{i});
+        if i == ncol
+            fprintf(fpw, " \\\\\n");
         else
-            fprintf(fpw, "& ");
+            fprintf(fpw, " & ");
         end
     end
 
     fprintf(fpw, "\t\t\\midrule\n");
 
-    %corpo da tabela
     for k = 1:nrow
         fprintf(fpw, "\t\t");
         for j = 1:ncol
-            fprintf(fpw, "%.2f", T{k, j});
+            val = T{k, j};
+            if isnumeric(val)
+                fprintf(fpw, "%.2f", val);
+            elseif iscell(val)
+                fprintf(fpw, "%s", string(val{1}));
+            else
+                fprintf(fpw, "%s", string(val));
+            end
             
-            if (j == ncol)
-                fprintf(fpw, "\\\\ \n");
+            if j == ncol
+                fprintf(fpw, " \\\\\n");
             else
                 fprintf(fpw, " & ");
             end
         end
     end
 
-    %rodapé da tabela
     fprintf(fpw, "\t\t\\bottomrule\n");
     fprintf(fpw, "\t\\end{tabular}\n");
-    fprintf(fpw, "\\end{table}");
-
+    fprintf(fpw, "\\end{table}\n");
 
     fclose(fpw);
 end
